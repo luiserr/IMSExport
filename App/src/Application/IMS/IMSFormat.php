ef1f0c01f21e859c0bfd12636dbbf93062fa6df3
Luis Rivero <luiseduardoriveroromero@gmail.com>
2022-08-08 22:49:01 -0500

    add ims generatpr

---
+++ App/src/Application/IMS/IMSFormat.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace IMSExport\Application\IMS;
+
+use IMSexport\Application\XMLGenerator\Generator;
+
+class IMSFormat
+{
+    protected Generator $XMLGenerator;
+
+    public function __construct()
+    {
+        $this->XMLGenerator = new Generator();
+    }
+
+    public function createMetadata(string $title, string $description): self
+    {
+        $this->XMLGenerator->createElement(
+            'metadata',
+            null,
+            null,
+            function (Generator $generator) use ($title, $description) {
+                $generator
+                    ->createElement('schema', null, 'IMS Common Cartridge')
+                    ->createElement('version', null, '1.0.0', null)
+                    ->createElement('lom', null, null, function (Generator $generator) use ($title, $description) {
+                        $generator->createElement('general', null, null, function (Generator $generator) use ($title, $description) {
+                            $generator->createElement('title', null, null, function (Generator $generator) use ($title) {
+                                $generator->createElement('string', null, $title, null);
+                            })
+                                ->createElement('description', null, null, function (Generator $generator) use ($description) {
+                                    $generator->createElement('string', null, $description, null);
+                                });
+                        });
+                    });
+            }
+        );
+        return $this;
+    }
+
+    public function createOrganization()
+    {
+
+    }
+
+    public function createOrganizations()
+    {
+
+    }
+
+    public function createItem()
+    {
+
+    }
+
+    public function createResource()
+    {
+
+    }
+
+    public function export()
+    {
+
+    }
+}
